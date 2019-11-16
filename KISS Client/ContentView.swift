0ed15b7c66d7f4acfc76530ad1b7cce5ba0c10ff
uma0317 <blog3170@gmail.com>
2019-11-16 22:49:31 +0900

    Initial Commit

---
+++ KISS Client/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KISS Client
+//
+//  Created by yuuma0317 on 2019/11/16.
+//  Copyright © 2019 yuuma0317. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
